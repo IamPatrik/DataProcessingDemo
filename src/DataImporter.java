6bbe7610c7e5e137230692b912819cb3039992ef
Patrik Ek <ek.patrik@gmail.com>
2015-08-04 16:50:54 +0200

    Commit stuff

---
+++ src/DataImporter.java
@@ -0,0 +1,11 @@
+/**
+ * File which takes a file and imports data into a given format
+ * 
+ * @author patrikek
+ *
+ */
+
+import org.apache.poi.hssf.usermodel.*;
+public class DataImporter {
+
+}
---
+++ src/DataProcessor.java
@@ -0,0 +1,8 @@
+
+public class DataProcessor {
+
+	public static void main(String[] args) {
+		System.out.println("Cannot run yet");
+	}
+
+}
